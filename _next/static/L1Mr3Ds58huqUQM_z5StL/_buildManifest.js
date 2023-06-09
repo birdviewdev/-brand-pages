b0f60d9fa24cab88bf16b51f3819a2d1c52af902
방 창배 <103403878+Changbae-Bang-BV@users.noreply.github.com>
2023-06-09 22:35:55 +0900

    효과 정리

---
+++ _next/static/L1Mr3Ds58huqUQM_z5StL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/964-6dd0ba1754ddcbe4.js","static/chunks/pages/index-8de3db702c062029.js"],"/_error":["static/chunks/pages/_error-065c0b5c19460b96.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
