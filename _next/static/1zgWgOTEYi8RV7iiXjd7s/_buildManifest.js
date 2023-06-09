858d06d6d5ce9fd8d39e01de57b327b87b711926
방 창배 <103403878+Changbae-Bang-BV@users.noreply.github.com>
2023-06-09 22:14:36 +0900

    에니메이션 추가

---
+++ _next/static/1zgWgOTEYi8RV7iiXjd7s/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/964-6dd0ba1754ddcbe4.js","static/chunks/pages/index-84bf2b254bb74fea.js"],"/_error":["static/chunks/pages/_error-065c0b5c19460b96.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
