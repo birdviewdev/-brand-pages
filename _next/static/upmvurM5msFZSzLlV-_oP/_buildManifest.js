b42e2046a0e9288724d08cd3e36d540587cbb484
방 창배 <103403878+Changbae-Bang-BV@users.noreply.github.com>
2023-06-09 15:47:25 +0900

    PC 까지 지원
    힘이 후달려서 반응형은 예외..처리...

---
+++ _next/static/upmvurM5msFZSzLlV-_oP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/655-f7d8a54d3ecb8a25.js","static/chunks/pages/index-079183ec06fce371.js"],"/_error":["static/chunks/pages/_error-065c0b5c19460b96.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
