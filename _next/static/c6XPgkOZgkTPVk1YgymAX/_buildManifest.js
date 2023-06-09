997c5fae9e4a421956697aeb30d32aa5c6e2d631
방 창배 <103403878+Changbae-Bang-BV@users.noreply.github.com>
2023-06-09 11:09:15 +0900

    스크롤 잡았다.

---
+++ _next/static/c6XPgkOZgkTPVk1YgymAX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/655-56e5a63d86d3ad38.js","static/chunks/pages/index-e2b767ce35e139f5.js"],"/_error":["static/chunks/pages/_error-065c0b5c19460b96.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
