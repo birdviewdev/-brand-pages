f4404a701adaa465cad3c8996667b72c240404e8
방 창배 <103403878+Changbae-Bang-BV@users.noreply.github.com>
2023-06-09 20:19:49 +0900

    딥링크 에러 처리
    그냥 복붙 하지 말자 다 왔다.

---
+++ _next/static/zaA3kcuAiOQRA0anHy0Z1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-f37ef9552ac8ad1a.js"],"/_error":["static/chunks/pages/_error-065c0b5c19460b96.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
