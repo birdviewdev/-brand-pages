b088b59c37052f4874a9ac1d9b7d64c70dd28a15
방 창배 <103403878+Changbae-Bang-BV@users.noreply.github.com>
2023-06-12 13:32:14 +0900

    모바일 높이 수정 들어갑니다.

---
+++ _next/static/AUuT4vQPNc8QTatKMLPIf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/33-623b2e9915e3a614.js","static/chunks/pages/index-d7af1d6223881f42.js"],"/_error":["static/chunks/pages/_error-065c0b5c19460b96.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
