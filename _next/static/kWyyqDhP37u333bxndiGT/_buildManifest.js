3a0e11d9d42e1ced239175ec0b103cd3f0d6b263
방 창배 <103403878+Changbae-Bang-BV@users.noreply.github.com>
2023-06-12 11:28:04 +0900

    컨피그 수정... 제발 되라..

---
+++ _next/static/kWyyqDhP37u333bxndiGT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/33-5e748c9cd2fa4f2d.js","static/chunks/pages/index-4f749645606d3912.js"],"/_error":["static/chunks/pages/_error-065c0b5c19460b96.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
