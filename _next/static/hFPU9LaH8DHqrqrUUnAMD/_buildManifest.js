3dca53e13f8f6204a07566b7147aea8130f12b8c
방 창배 <103403878+Changbae-Bang-BV@users.noreply.github.com>
2023-06-12 12:56:38 +0900

    실험..
    https://wallis.dev/blog/next-js-basepath-and-assetprefix

---
+++ _next/static/hFPU9LaH8DHqrqrUUnAMD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/33-fef0bf3d47ad93e7.js","static/chunks/pages/index-261d44833a9d055f.js"],"/_error":["static/chunks/pages/_error-065c0b5c19460b96.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
