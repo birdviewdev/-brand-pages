7bcb9bd327775abcb5d337f2204b3d7b751c7fb3
방 창배 <103403878+Changbae-Bang-BV@users.noreply.github.com>
2023-06-12 21:15:27 +0900

    is view 를 활용하여 처리

---
+++ _next/static/dakCY1V5QVQT6--xhkf1G/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/33-623b2e9915e3a614.js","static/chunks/pages/index-febe9815e4b90819.js"],"/_error":["static/chunks/pages/_error-065c0b5c19460b96.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
