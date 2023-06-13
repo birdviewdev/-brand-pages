1f4ae16c10b4645c082a7675dc27e9eff820f151
방 창배 <103403878+Changbae-Bang-BV@users.noreply.github.com>
2023-06-13 13:40:51 +0900

    아차.. prd..

---
+++ _next/static/10C12_8pIkxM2luRzg2S9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/33-623b2e9915e3a614.js","static/chunks/pages/index-544e184a83f974e2.js"],"/_error":["static/chunks/pages/_error-1626861c6fc481ac.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
